022d5bd98af841e533a5f5f28e30af8ad6c5ef6d
Marc Jou <smyslov@gmail.com>
2023-12-01 18:59:29 +0100

    Some refactor

--- go/day01.go
+++ go/day01.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
@@ -15,26 +14,26 @@ var values = map[string]int{
 	"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
 }
 
-func extractNums(line string) int {
-	pattern := regexp.MustCompile(`(\d)`)
-	matches := pattern.FindAllStringSubmatch(line, -1)
-	first, _ := strconv.Atoi(matches[0][0])
-	last, _ := strconv.Atoi(matches[len(matches)-1][0])
-
-	return first*10 + last
-}
-
 func convert(val string) int {
 	return values[val]
 }
 
-func extractComplex(line string) int {
-	pattern := regexp.MustCompile(`(zero|one|two|three|four|five|six|seven|eight|nine|\d)`)
+func extractAny(line string, pattern *regexp.Regexp) int {
 	matches := pattern.FindAllStringSubmatch(line, -1)
 	first := convert(matches[0][0])
 	last := convert(matches[len(matches)-1][0])
 
 	return first*10 + last
+}
+
+func extractNums(line string) int {
+	pattern := regexp.MustCompile(`(\d)`)
+	return extractAny(line, pattern)
+}
+
+func extractComplex(line string) int {
+	pattern := regexp.MustCompile(`(zero|one|two|three|four|five|six|seven|eight|nine|\d)`)
+	return extractAny(line, pattern)
 }
 
 func solve(data string, exctractfn extract) int {
